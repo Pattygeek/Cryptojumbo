29a6852049db4db473d0c99de7a194e04b7cf95d
charles0818 <charles081889@gmail.com>
2021-01-22 17:15:51 +0100

    utils: App Utilities - Bank Codes - Money and Time Formats - Chakra Theming

--- src/utils/formats/index.ts
+++ src/utils/formats/index.ts
@@ -1,7 +1,32 @@
-export const formatAmount = (amount: number): string => {
-  const formatter: string = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(amount);
+export const formatAmount = (amount: number, currency?: 'NGN' | 'USD'): string => {
+  const initializeCurrency = currency || 'USD';
+  const currencySymbol: any = {
+    NGN: {
+      locale: 'en-NG',
+      currency: 'NGN',
+    },
+    USD: {
+      local: 'en-US',
+      currency: 'USD',
+    },
+  };
+  const formatter: string = new Intl.NumberFormat(
+    currencySymbol[initializeCurrency].locale,
+    {
+      style: 'currency',
+      currency: currencySymbol[initializeCurrency].currency,
+    },
+  ).format(amount);
+  return formatter;
+};
+
+export const formatDate = (timestamp: string): string => {
+  if (!timestamp) return '';
+  const date = new Date(timestamp);
+  const formatter = new Intl.DateTimeFormat('en-NG', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }).format(date);
   return formatter;
 };
