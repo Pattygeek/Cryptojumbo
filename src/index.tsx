798822042bcbeb45b7ded71897ed22a7599ac156
charles0818 <charles081889@gmail.com>
2021-01-22 19:01:20 +0100

    App Entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import webFont from 'webfontloader';
+import { CustomTheme } from './utils';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux';
+webFont.load({
+  google: {
+    families: ['Poppins:400:500,', 'sans-serif'],
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={CustomTheme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
