9e82cda688a4c321a8bfb506a10962adab3dd76b
charles0818 <charles081889@gmail.com>
2021-01-22 16:55:23 +0100

    feat: Landing Page UI - Coming Soon Section - FAQ Section - Brief About Us Section

---
+++ src/pages/Home/components/Faq.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { SectionWrapper } from '../../components';
+import { Flex, Image, Text, Box, Stack, Center } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+export const FAQs: React.FC = () => {
+  return (
+    <SectionWrapper mt={50}>
+      <Text
+        textAlign="center"
+        className="font-md color-gray-text font-weight-600"
+        mb={10}>
+        Frequently Asked Questions
+      </Text>
+      <Stack spacing={10} mb={5} direction={{ base: 'column', md: 'row' }}>
+        <FAQ
+          question="What is CryptoJumbo?"
+          answer="CryptoJumbo is a digital cryptocurrency platform where you can sell your bitcoin at an affordable rate."
+        />
+        <FAQ
+          question="What is Bitcoin?"
+          answer="Bitcoin is a cryptocurrency invented in 2008 by an unknown person or group of people using the name Satoshi Nakamoto and started in 2009 when its implementation was released as open-source software."
+        />
+        <FAQ
+          question="What is Etherum?"
+          answer="Ethereum is a decentralized open source blockchain featuring smart contract functionality. It is the second-largest cryptocurrency by market capitalization, behind Bitcoin"
+        />
+      </Stack>
+      <Center>
+        <Box
+          as={Link}
+          to="/faqs"
+          borderRadius={5}
+          className="bg-white color-dark font-weight-600 font-sm"
+          p={3}>
+          Read more FAQs
+        </Box>
+      </Center>
+    </SectionWrapper>
+  );
+};
+
+declare interface FAQProps {
+  question: string;
+  answer: string;
+}
+const FAQ: React.FC<FAQProps> = ({ question, answer }) => {
+  return (
+    <Box flex={1} className="bg-primary-gradient-opacity" p={5}>
+      <Text className="font-md color-dark font-weight-600" mb={5}>
+        {question}
+      </Text>
+      <Text className="color-dark font-sm font-weight-400">{answer}</Text>
+    </Box>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,166 +1,269 @@
 import React from 'react';
-import { Box, Flex, Text, Image } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Text,
+  Image,
+  AspectRatio,
+  Stack,
+  Center,
+} from '@chakra-ui/react';
 import {
   NavigationTop,
   AvailableCoinsSmallBox,
   AvailableCoinsLargeBox,
+  PreFooter,
+  Footer,
+  SectionWrapper,
 } from '../components';
 import { AiOutlineStar } from 'react-icons/ai';
-import secure from '../../assets/secure.svg';
+import secure from '../../assets/secure.png';
 import wallet from '../../assets/wallet.svg';
 import payout from '../../assets/payout.svg';
 import arrowDown from '../../assets/arrow-down.svg';
+import currencies from '../../assets/currency-group.png';
+import asteriks from '../../assets/asteriks.png';
+import cheque from '../../assets/cheque.png';
+import money from '../../assets/money.png';
+import { WhyTrustUs, ComingSoon, FAQs } from './components';
 
 const Home: React.FC = (): JSX.Element => {
   return (
     <Box>
-      <Box as="header" className="header" p="20px 40px">
+      <Box
+        as="header"
+        className="header"
+        p={{ base: '0px', sm: '15px 20px', md: '20px 40px' }}
+        mb={{ base: '70%', sm: '40%', md: '20%' }}>
         <NavigationTop />
-        <Box mb={5}>
-          <Flex direction="column" px={30} mt={40}>
-            <Flex>
-              <Box flex={1} mr={10}>
-                <Text className="font-xlg color-dark font-weight-600">
-                  Buy, Sell and swap Cryptocurrency with ease
-                </Text>
-                <Text className="color-white font-sm font-weight-400">
-                  Fast payout | Secured Trade | Affordable rate
-                </Text>
-                <AvailableCoinsSmallBox />
-              </Box>
-              <Box flex={1}>
-                <AvailableCoinsLargeBox />
-              </Box>
-            </Flex>
-          </Flex>
-        </Box>
-        <Box
-          borderRadius={15}
-          boxShadow="0px 5px 20px rgba(20, 102, 204, 0.15)"
-          className="bg-white"
-          py={10}
-          px={20}
-          transform="translateY(40%)">
-          <Text
-            textAlign="center"
-            mb={4}
-            className="color-gray-heading font-lg font-weight-500">
-            CryptoJumbo offers
-          </Text>
-          <Flex direction={{ sm: 'column', md: 'row' }}>
-            <Flex flex={1}>
-              <Flex flex={1} direction="column" align="center" justify="center">
-                <Image src={secure} mb={2} />
-                <Text
-                  className="color-gray-text font-md font-weight-400"
-                  textAlign="center">
-                  Easy{' '}
-                  <Text className="color-primary font-weight-600">
-                    Secured transaction
+        <Box px={{ base: 5, sm: 15, md: 30, lg: '80px' }}>
+          <Box mb={5}>
+            <Flex direction="column" mt={40}>
+              <Flex direction={{ base: 'column', md: 'row' }}>
+                <Box flex={0.45} mr={{ base: '0px', md: 10 }}>
+                  <Text
+                    textAlign={{ base: 'center', md: 'left' }}
+                    mb={2}
+                    pr="20px"
+                    className="font-xlg color-dark font-weight-600"
+                    lineHeight="31px">
+                    Buy, Sell and swap Cryptocurrency with ease
                   </Text>
-                </Text>
-              </Flex>
-              <Flex flex={1} direction="column" align="center" justify="center">
-                <Image src={payout} mb={2} />
-                <Text
-                  className="color-primary font-md font-weight-600"
-                  textAlign="center">
-                  Faster payout{' '}
-                  <Text className="color-gray-text font-weight-400">
-                    on all transactions
+                  <Text
+                    textAlign={{ base: 'center', md: 'left' }}
+                    className="color-white font-slg font-weight-400">
+                    Fast Payout | Secured Trade | Affordable rate
                   </Text>
-                </Text>
-              </Flex>
-            </Flex>{' '}
-            <Flex flex={1}>
-              <Flex flex={1} direction="column" align="center" justify="center">
-                <Image src={arrowDown} mb={2} />
-                <Text
-                  className="color-gray-text font-md font-weight-400"
-                  textAlign="center">
-                  up-to-date and{' '}
-                  <Text className="color-primary font-weight-600">
-                    affordable rate
-                  </Text>
-                </Text>
-              </Flex>
-              <Flex flex={1} direction="column" align="center" justify="center">
-                <Image src={wallet} mb={2} />
-                <Text
-                  className="color-primary font-md font-weight-600"
-                  textAlign="center">
-                  Easy{' '}
-                  <Text className="color-gray-text font-weight-400">
-                    Secured transaction
-                  </Text>
-                </Text>
+                  <AvailableCoinsSmallBox />
+                </Box>
+                <Box flex={0.55}>
+                  <AvailableCoinsLargeBox />
+                </Box>
               </Flex>
             </Flex>
-          </Flex>
+          </Box>
+          <Box
+            borderRadius={15}
+            boxShadow="0px 5px 20px rgba(20, 102, 204, 0.15)"
+            className="bg-white offers"
+            py={{ base: '40px', md: '60px' }}
+            px={{ base: 5, md: 20 }}
+            transform="translateY(50%)">
+            <Text
+              textAlign="center"
+              mb={'30px'}
+              className="color-gray-heading font-lg font-weight-500">
+              CryptoJumbo offers
+            </Text>
+            <Flex spacing={5} direction={{ base: 'column', md: 'row' }}>
+              <Flex flex={1} mb={5}>
+                <Stack
+                  flex={1}
+                  spacing={{ base: 5, md: 10 }}
+                  direction="column"
+                  align="center"
+                  justify="center">
+                  <Image src={secure} mb={2} />
+                  <Box display="inline-block" lineHeight="24px">
+                    <Text
+                      display="inline"
+                      className="color-gray-text font-md font-weight-400"
+                      textAlign="center"
+                      mr={1}>
+                      Easy &
+                    </Text>
+                    <Text
+                      display="inline"
+                      className="color-primary font-md font-weight-600">
+                      Secured
+                    </Text>
+                    <Text
+                      textAlign="center"
+                      className="color-primary font-md font-weight-600">
+                      transactions
+                    </Text>
+                  </Box>
+                </Stack>
+                <Stack
+                  flex={1}
+                  spacing={{ base: 5, md: 10 }}
+                  direction="column"
+                  align="center"
+                  justify="center">
+                  <Image src={payout} mb={2} />
+                  <Box display="inline-block" lineHeight="24px" className="font-md">
+                    <Text
+                      display="inline"
+                      className="color-primary font-md font-weight-600"
+                      textAlign="center"
+                      mr={2}>
+                      Faster Payouts
+                    </Text>
+                    <Text
+                      display="inline"
+                      className="color-gray-text font-md font-weight-400"
+                      textAlign="center">
+                      on
+                    </Text>
+                    <Text
+                      textAlign="center"
+                      className="color-gray-text font-weight-400">
+                      all transactions
+                    </Text>
+                  </Box>
+                </Stack>
+              </Flex>{' '}
+              <Flex flex={1} mb={5}>
+                <Stack
+                  flex={1}
+                  spacing={{ base: 5, md: 10 }}
+                  direction="column"
+                  align="center"
+                  justify="center">
+                  <Image src={arrowDown} mb={2} />
+                  <Text
+                    className="color-gray-text font-md font-weight-400"
+                    textAlign="center">
+                    up-to-date &{' '}
+                    <Text className="color-primary font-weight-600">
+                      affordable rate
+                    </Text>
+                  </Text>
+                </Stack>
+                <Stack
+                  flex={1}
+                  spacing={{ base: 5, md: 10 }}
+                  direction="column"
+                  align="center"
+                  justify="center">
+                  <Image src={wallet} mb={2} />
+                  <Box display="inline-block" lineHeight="24px">
+                    <Text
+                      display="inline"
+                      className="color-gray-text font-md font-weight-400"
+                      textAlign="center"
+                      mr={1}>
+                      We Provide
+                    </Text>
+                    <Text
+                      display="inline"
+                      className="color-primary font-md font-weight-600">
+                      Multiple
+                    </Text>
+                    <Text
+                      textAlign="center"
+                      className="color-primary font-md font-weight-600">
+                      Crypto-Wallet
+                    </Text>
+                  </Box>
+                </Stack>
+              </Flex>
+            </Flex>
+          </Box>
         </Box>
       </Box>
       <HowItWorks />
+      <WhyTrustUs />
+      <ComingSoon />
+      <FAQs />
+      <PreFooter />
+      <Footer />
     </Box>
   );
 };
 
 const HowItWorks: React.FC = (): JSX.Element => {
   return (
-    <Box>
-      <Text as="h2" className="uppercase color-gray-heading font-md font-weight-500">
-        HOW IT WORKS
-      </Text>
-      <Text as="h3" className="font-weight-400 font-sm color-gray-text">
-        Buying, selling and swapping cryptocurrency can be simple, easy and secure
-        with Crypto Jumbo
-      </Text>
-      <Flex>
-        <Box maxWidth={262} flex={1} p={3} className="how-it-works-step">
-          <Flex maxWidth={55} className="padding-sm icon-container" borderRadius={5}>
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-          </Flex>
-          <Text className="font-sm font-weight-400 color-white">
+    <SectionWrapper>
+      <Center flexDirection="column" mb={5}>
+        <Box maxWidth="550px">
+          <Text
+            as="h2"
+            className="uppercase color-gray-heading font-slg font-weight-500"
+            textAlign="center"
+            mb={5}>
+            HOW IT WORKS
+          </Text>
+          <Text
+            as="h3"
+            className="font-weight-400 font-md color-gray-text"
+            textAlign="center">
+            Buying, selling and swapping cryptocurrency can be simple, easy and
+            secure with Crypto Jumbo
+          </Text>
+        </Box>
+      </Center>
+      <Stack spacing={0} direction={{ base: 'column', sm: 'row' }}>
+        <Stack
+          direction={{ base: 'row', sm: 'column' }}
+          className="how-it-works-step"
+          spacing={5}
+          flex={0.3}
+          p={7}
+          borderTopLeftRadius={5}
+          borderBottomLeftRadius={5}>
+          <Image src={asteriks} width="60px" height="auto" alt="Security" />
+          <Text className="font-md font-weight-400 color-white" lineHeight="24px">
             Login or sign up to your trading dashboard
           </Text>
-        </Box>
-        <Box maxWidth={262} flex={1} p={3} className="how-it-works-step">
-          <Flex maxWidth={55} className="padding-sm icon-container" borderRadius={5}>
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-          </Flex>
-          <Text className="font-sm font-weight-400 color-white">
+        </Stack>
+        <Stack
+          direction={{ base: 'row', sm: 'column' }}
+          className="how-it-works-step"
+          spacing={5}
+          flex={0.3}
+          p={7}>
+          <Image src={currencies} width="60px" height="auto" alt="Currencies" />
+          <Text className="font-md font-weight-400 color-white" lineHeight="24px">
             Select digital currency to Buy, Sell or Swap
           </Text>
-        </Box>
-        <Box maxWidth={262} flex={1} p={3} className="how-it-works-step">
-          <Flex maxWidth={55} className="padding-sm icon-container" borderRadius={5}>
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-          </Flex>
-          <Text className="font-sm font-weight-400 color-white">
+        </Stack>
+        <Stack
+          direction={{ base: 'row', sm: 'column' }}
+          className="how-it-works-step"
+          spacing={5}
+          flex={0.3}
+          p={7}>
+          <Image src={cheque} width="60px" height="auto" alt="Cheque" />
+          <Text className="font-md font-weight-400 color-white" lineHeight="24px">
             Complete trading procedures
           </Text>
-        </Box>
-        <Box maxWidth={262} flex={1} p={3} className="how-it-works-step">
-          <Flex maxWidth={55} className="padding-sm icon-container" borderRadius={5}>
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-            <AiOutlineStar className="font-sm icon margin-right-xs" />
-          </Flex>
-          <Text className="font-sm font-weight-400 color-white">
+        </Stack>
+        <Stack
+          direction={{ base: 'row', sm: 'column' }}
+          className="how-it-works-step"
+          spacing={5}
+          flex={0.3}
+          p={7}
+          borderTopRightRadius={5}
+          borderBottomRightRadius={5}>
+          <Image src={money} width="60px" height="auto" alt="Cheque" />
+          <Text className="font-md font-weight-400 color-white" lineHeight="24px">
             Recieve payment or crypto-currency
           </Text>
-        </Box>
-      </Flex>
-    </Box>
+        </Stack>
+      </Stack>
+    </SectionWrapper>
   );
 };
 export default Home;
